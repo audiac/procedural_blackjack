c5c35ec227a78468f683f3281a76b5a663c23280
Ryan <ryan.geo@gmail.com>
2015-03-03 07:37:07 -0800

    Remove long else condition and exit if blackjack

--- blackjack.rb
+++ blackjack.rb
@@ -23,6 +23,7 @@ end
 
 def has_blackjack?(hand)
   cards = hand.flatten
+  # check if cards include an Ace and 10, 'J', 'Q', 'K'
   if cards.include?('A') && (cards.include?('10') || cards.include?('J') ||
      cards.include?('Q') || cards.include?('K'))
     return true
@@ -52,57 +53,58 @@ dealer_cards << deck.pop
 # check if either player has blackjack
 if has_blackjack?(player_cards)
   puts "#{player_name} has Blackjack with #{player_cards}!"
+  exit
 elsif has_blackjack?(dealer_cards)
   puts "Dealer has Blackjack with #{dealer_cards}!"
-# if neither player has blackjack, play game
-else
-  player_hand_total = calculate_card_total(player_cards)
-  dealer_hand_total = calculate_card_total(dealer_cards)
+  exit
+end
 
-  # player turn
-  loop do
-    puts "Dealer showing: #{dealer_cards[0]}"
-    puts "#{player_name}'s hand: #{player_cards}"
-    puts "You have #{player_hand_total}."
-    puts "\nWould you like to hit or stay? (h/s)"
-    hit_or_stay = gets.chomp.downcase
+player_hand_total = calculate_card_total(player_cards)
+dealer_hand_total = calculate_card_total(dealer_cards)
 
-    if hit_or_stay == 'h'
-      player_cards << deck.pop
-      player_hand_total = calculate_card_total(player_cards)
-      if player_hand_total > 21
-        puts "#{player_name} busted with #{player_cards}!"
-        exit
-      end
-    else
-      puts "#{player_name} chooses to stay."
-      break
-    end
-  end
+# player turn
+loop do
+  puts "Dealer showing: #{dealer_cards[0]}"
+  puts "#{player_name}'s hand: #{player_cards}"
+  puts "You have #{player_hand_total}."
+  puts "\nWould you like to hit or stay? (h/s)"
+  hit_or_stay = gets.chomp.downcase
 
-  # dealer turn
-  while dealer_hand_total < 17
-    puts "Dealer hits."
-    dealer_cards << deck.pop
-    dealer_hand_total = calculate_card_total(dealer_cards)
-    puts "Dealer has #{dealer_hand_total}."
-
-    if dealer_hand_total > 21
-      puts "Dealer busted with #{dealer_cards}"
-      puts "You win!"
+  if hit_or_stay == 'h'
+    player_cards << deck.pop
+    player_hand_total = calculate_card_total(player_cards)
+    if player_hand_total > 21
+      puts "#{player_name} busted with #{player_cards}!"
       exit
     end
-  end
-
-  puts "Dealer stays."
-
-  # determine winner
-  puts "Dealer has #{dealer_hand_total} with #{dealer_cards}"
-  if dealer_hand_total == 21 || (dealer_hand_total > player_hand_total)
-    puts "Dealer wins!"
-  elsif player_hand_total > dealer_hand_total
-    puts "#{player_name} wins!"
   else
-    puts "Hands are equal. Push!"
+    puts "#{player_name} chooses to stay."
+    break
   end
+end
+
+# dealer turn
+while dealer_hand_total < 17
+  puts "Dealer hits."
+  dealer_cards << deck.pop
+  dealer_hand_total = calculate_card_total(dealer_cards)
+  puts "Dealer has #{dealer_hand_total}."
+
+  if dealer_hand_total > 21
+    puts "Dealer busted with #{dealer_cards}"
+    puts "You win!"
+    exit
+  end
+end
+
+puts "Dealer stays."
+
+# determine winner
+puts "Dealer has #{dealer_hand_total} with #{dealer_cards}"
+if dealer_hand_total == 21 || (dealer_hand_total > player_hand_total)
+  puts "Dealer wins!"
+elsif player_hand_total > dealer_hand_total
+  puts "#{player_name} wins!"
+else
+  puts "Hands are equal. Push!"
 end
